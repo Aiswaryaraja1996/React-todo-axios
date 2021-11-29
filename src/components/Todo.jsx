0e31e93db8765df82de1cd90538d4d787ac6cadf
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2021-11-30 00:23:56 +0530

    todo-axios

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -24,6 +24,27 @@ const createTodo = (title) => {
   return axios(config);
 };
 
+const patchTodo = (title, id, status) => {
+  const payLoad1 = {
+    title,
+    status: !status,
+  };
+  const config = {
+    url: `http://localhost:3001/data/${id}`,
+    method: "PATCH",
+    data: payLoad1,
+  };
+  return axios(config);
+};
+
+const deleteTodo = (id) => {
+  const config = {
+    url: `http://localhost:3001/data/${id}`,
+    method: "DELETE",
+  };
+  return axios(config);
+};
+
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [isLoading, setLoading] = useState(true);
@@ -51,6 +72,26 @@ const Todo = () => {
     }
   };
 
+  const handleToggle = async (title, id, status) => {
+    try {
+      setLoading(true);
+      await patchTodo(title, id, status);
+      await handleGetTodo();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      setLoading(true);
+      await deleteTodo(id);
+      await handleGetTodo();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   if (isLoading) {
     return (
       <div>
@@ -61,12 +102,30 @@ const Todo = () => {
 
   return (
     <div>
+      <h1>TODOS</h1>
       <TodoInput submit={handleSubmit}></TodoInput>
-      <br/>
+      <br />
       <div>
         {todos.map((item) => (
-          <div key={item.id}>
-            <div>{item.title} : {item.status? "Completed" : "Not Completed"}</div>
+          <div
+            key={item.id}
+            style={{
+              border: "1px solid black",
+              padding: "20px",
+              width: "20%",
+              margin: "10px auto",
+            }}
+          >
+            <div>
+              {item.title} : {item.status ? "Completed" : "Not Completed"}
+            </div>
+            <button
+              onClick={() => handleToggle(item.title, item.id, item.status)}
+              style={{ marginRight: "10px" }}
+            >
+              {!item.status ? "Mark Complete" : "Mark Uncomplete"}
+            </button>
+            <button onClick={() => handleDelete(item.id)}>Delete</button>
           </div>
         ))}
       </div>
